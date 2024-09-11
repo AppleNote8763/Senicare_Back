941dff1279c3dff2b3e83941434bb45768a7b8ca
AppleNote <a7201492425@gmail.com>
2024-09-11 17:08:58 +0900

    2024.09.11. 17:08

--- src/main/java/com/korit/senicare/repository/CustomerRepository.java
+++ src/main/java/com/korit/senicare/repository/CustomerRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.korit.senicare.entity.CustomerEntity;
 
 @Repository
-public interface CustomerRepository extends JpaRepository<CustomerEntity, String>{
+public interface CustomerRepository extends JpaRepository<CustomerEntity, Integer>{
     
 }
