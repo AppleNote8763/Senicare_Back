847eb90a491922a9d8e2d0b5a0fb5a9c28b15b30
AppleNote <a7201492425@gmail.com>
2024-09-24 17:18:59 +0900

    2024.09.24. 17:18

--- src/main/java/com/korit/senicare/service/NurseService.java
+++ src/main/java/com/korit/senicare/service/NurseService.java
@@ -2,10 +2,12 @@ package com.korit.senicare.service;
 
 import org.springframework.http.ResponseEntity;
 
+import com.korit.senicare.dto.response.nurse.GetNurseListResponseDto;
 import com.korit.senicare.dto.response.nurse.GetSignInResponseDto;
 
 public interface NurseService {
 
     ResponseEntity<? super GetSignInResponseDto> getSignIn(String userId);
+    ResponseEntity<? super GetNurseListResponseDto> getNurseList();
 
 }
