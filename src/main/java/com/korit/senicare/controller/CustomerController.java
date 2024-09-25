02bb3ecb161100738048eea9a18803b7fc6a6b4f
AppleNote <a7201492425@gmail.com>
2024-09-25 15:51:28 +0900

    2024.09.25. 15:51

--- src/main/java/com/korit/senicare/controller/CustomerController.java
+++ src/main/java/com/korit/senicare/controller/CustomerController.java
@@ -2,6 +2,7 @@ package com.korit.senicare.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -49,13 +50,22 @@ public class CustomerController {
         return response;
     }
 
-    @PatchMapping("{/customerNumber}")
+    @PatchMapping("/{customerNumber}")
     public ResponseEntity<ResponseDto> patchCustomer(
         @RequestBody @Valid PatchCustomerRequestDto requestBody, 
         @PathVariable("customerNumber") Integer customerNumber, 
         @AuthenticationPrincipal String userId
     ) {
         ResponseEntity<ResponseDto> response = customerService.patchCustomer(requestBody, customerNumber, userId);
+        return response;
+    }
+
+    @DeleteMapping("/{customerNumber}")
+    public ResponseEntity<ResponseDto> deleteCustomer(
+        @PathVariable("customerNumber") Integer customerNumber, 
+        @AuthenticationPrincipal String userId
+    ) {
+        ResponseEntity<ResponseDto> response = customerService.deleteCustomer(customerNumber, userId);
         return response;
     }
 
